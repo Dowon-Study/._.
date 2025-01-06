95e6e3ce0dbbd2c85612b47a6f31671154f78eb2
Dowon-Study <dowon_9898@naver.com>
2025-01-06 19:50:33 +0900

    백준10988번

--- 심화1/study1/src/Main.java
+++ 심화1/study1/src/Main.java
@@ -21,32 +21,54 @@
 
 
 //백준 2444번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+
+//         for(int i = 1; i <= num; i++){
+//             for(int j = 1; j <= num - i; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int k = 1; k <= 2*i - 1; k++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         for(int i = 1; i < num; i++){
+//             for(int j = 1; j <= i; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int k = 1; k <= 2*(num - i) - 1; k++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+
+//     }
+// }
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
 
-        for(int i = 1; i <= num; i++){
-            for(int j = 1; j <= num - i; j++){
-                System.out.print(" ");
-            }
-            for(int k = 1; k <= 2*i - 1; k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i = 1; i < num; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.print(" ");
-            }
-            for(int k = 1; k <= 2*(num - i) - 1; k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        sc.close();
+        String str = sc.next();
+        int len1 = str.length() - 1;
 
+        for(int i = 0; i < len1; i++){
+            if(str.charAt(i) != str.charAt(len1 - i)){
+                System.out.println(0);
+                break;
+            } else if (i > (int)len1 / 2){
+                System.out.println(1);
+                break;
+            }
+            
+        }
     }
 }
